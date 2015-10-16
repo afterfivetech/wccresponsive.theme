d4e21cacf0cdcc1fa24a5846fb8a7b043fc89f04
joemariedimzon <joemariedimzon@gmail.com>
2015-10-16 13:56:38 +0800

    adjustment for color scheme

--- wccresponsive/theme/setuphandlers.py
+++ wccresponsive/theme/setuphandlers.py
@@ -3,7 +3,6 @@ from plone.registry.interfaces import IRegistry
 from zope.component import getUtility
 from wccresponsive.theme.interfaces import IThemeSettings
 
-
 @gs.importstep(
     name=u'wccresponsive.theme', 
     title=u'wccresponsive.theme import handler',
