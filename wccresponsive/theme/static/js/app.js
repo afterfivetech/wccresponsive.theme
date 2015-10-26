7f9eca84a3a6ef3c3c2101a4111ab00d5a85343a
joemariedimzon <joemariedimzon@gmail.com>
2015-10-26 15:07:10 +0800

    adjustment for jquery conflict in large screen slider and foundation off canvas

--- wccresponsive/theme/static/js/app.js
+++ wccresponsive/theme/static/js/app.js
@@ -1,4 +1,5 @@
-$(document).foundation();
+//$(document).foundation();
+jQuery(document).foundation();
 $( "#button-search" ).click(function() {
   $( "#effect" ).toggleClass( "show-for-large" );
 });
